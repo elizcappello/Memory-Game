069e9e7c5e8989834ec5bf3afebdec27dfe275b8
Elizabeth Cappello <ejd@elizabethjanedesigns.com>
2018-07-11 09:51:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 /*
  * Create a list that holds all of your cards
  */
+var cards = ['paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'cube', 'cube',
+        'anchor', 'anchor', 'leaf', 'leaf ', 'bicycle ', 'bicycle ', 'diamond ', 'diamond ',
+        'bomb', 'bomb', 'leaf', 'leaf'
+    ],
+
+    function generateCard(card) {
+        return '<li class="card" data-card= "${card}"><i class = "fa ${card}"></i></li>';
+
+    }
 
 
 /*
@@ -12,7 +21,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +34,87 @@ function shuffle(array) {
 
     return array;
 }
+
+//Begin game and select cards
+function initGame() {
+    var deck = document.querySelector('.deck');
+
+
+    //Shuffle the array
+    var cardHTML = shuffle(cards).map(function(card) {
+        return generateCard(card);
+    });
+    moves = 0;
+    moveCounter.innerText = moves;
+
+    deck.innerHTML = cardHTML.join('');
+
+}
+
+
+var allCards = document.querySelectorAll('.card'); /*selects all the cards*/
+var openCards = []; /*make an array where all of the cards are stored*/
+var moves = 0;
+
+initGame();
+
+allCards.forEach(function(card) { /*loop through the cards*/
+    card.addEventListener('click', function(e) { /*add an event listener*/ ///card list
+
+        if (!card.classList.contains('open') && !card.classList.contains('show)') && !card.classList.contains('match')) {
+            openCards.push(card); /*pushes the current card into the array*/
+            card.classList.add('open', 'show'); /*makes all of teh cards turn over*/
+
+
+            //check if they match
+            //var firstCardType = openCards[0].dataset.card;
+            //console.log(firstCardType);
+
+
+
+            if (openCards.length == 2) { //hide
+                if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                    openCards[0].classList.add('match');
+                    openCards[0].classList.add('open');
+                    openCards[0].classList.add('show');
+
+                    openCards[1].classList.add('match');
+                    openCards[1].classList.add('open');
+                    openCards[1].classList.add('show');
+
+                    openCards = [];
+
+                } else {
+
+
+                }
+
+                //if cards don't match go away,hide
+                setTimeout(function() { /*after a period of time will hide the cards*/
+                    openCards.forEach(function(card) { /*loops through the cards*/
+                        card.classList.remove('open', 'show'); /*hide the cards*/
+
+                    });
+
+                    openCards = [];
+
+                }, 1000);
+
+            }
+
+            moves += 1;
+            moveCounter.innertext.moves;
+
+        }
+
+    });
+
+});
+
+
+
+
+
 
 
 /*
